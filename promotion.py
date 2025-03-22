9fcc47ddd30aa1b9a4427fcba3e318c8a13dc01c
Snehitha2003M <mallelasnehitha@gmail.com>
2025-03-22 04:20:08 +0000

    prommotion status

---
+++ promotion.py
@@ -0,0 +1,14 @@
+S1=35
+S2=50
+S3=70
+if(S1>=35):
+    if(S2>=35):
+        if(S3>=35):
+            print("promoted")
+        else:
+            print("Not Promoted")
+    else:
+        print("Not Promoted")
+else:
+    print("Not Promoted")
+    
