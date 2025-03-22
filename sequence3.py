37e3cc7e8d9d500ca9036512d636b96dfd50b2ec
Snehitha2003M <mallelasnehitha@gmail.com>
2025-03-22 08:48:04 +0000

    sequence3

--- sequence3.py
+++ sequence3.py
@@ -1,6 +1,6 @@
 n=5
 for i in range(n):
-    print(" "*i,end=" ")
-    for j in range(n,0,-1):
-        print(j,end=" ")
+    print(" "*i,end="")
+    for j in range(1,n-i+1):
+        print(j,end="")
     print()
